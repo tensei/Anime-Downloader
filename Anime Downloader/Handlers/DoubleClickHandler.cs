3f826fc69c0d35db085215b7b75e6dd812f1d303
Davut <davuttensei@gmail.com>
2015-08-08 22:07:00 +0200

    code cleanup

--- Anime Downloader/Handlers/DoubleClickHandler.cs
+++ Anime Downloader/Handlers/DoubleClickHandler.cs
@@ -1,22 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows.Controls;
-using System.Windows.Media;
 
-namespace Anime_Downloader
+namespace Anime_Downloader.Handlers
 {
     class DoubleClickHandler
     {
-        private readonly SolidColorBrush ReadColorFg = new SolidColorBrush(Color.FromArgb(255, 140, 140, 140));
-        public void Open(ListBoxItem item)
+        public bool Open(ListBoxItem item)
         {
-            if (!item.Tag.Equals("blank"))
-                Process.Start(item.Tag.ToString());
-            item.Foreground = ReadColorFg;
+            if (item.Tag.Equals("blank")) return false;
+            Process.Start(item.Tag.ToString());
+            return true;
         }
     }
 }
